273389b05ef115bc31c3a9cdfa586bba9e68c136
Kidcreole2 <malakhin2332@gmail.com>
2024-04-30 22:01:08 +0500

    hub page added

--- fullstack/routes.py
+++ fullstack/routes.py
@@ -1,5 +1,5 @@
 from flask import request, render_template, redirect, url_for
-from flask_login import logout_user
+from flask_login import logout_user, current_user, login_required
 from core import app, login_manager
 from models import Users
 
@@ -28,35 +28,41 @@ def register():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        login = request.form.get("login")
+        #TODO make password check
+        login = request.form["login"]
         print(login)
         password = request.form.get("password")
         role = Users.auth_user(login, password)
+    
+        if role == "student":
+            return render_template("pages/student/index.html")
         
-        match role:
-            case "admin":
-                return render_template("pages/admin/index.html")
-            case "opop_supervisor":
-                return render_template("pages/opop/index.html")
-            case "practice_supervisor":
-                return render_template("pages/Teacher/index.html")
-            case "student":
-                return render_template("pages/student/index.html")
-            case "wrong_login":
-                return render_template("login.html")
-            case "wrong_pass":
-                return render_template("login.html")
-    return render_template("login.html", {role: True})
+        return redirect(url_for("home"))
 
 @app.route("/logout")
+@login_required
 def logout():
     logout_user()
     return redirect(url_for("home"))
 
-@app.route("/opop_reduction")
-def opop_reduction():
-    return render_template("pages/opop/create.html")
+@app.route("/opop")
+@login_required
+def opop_page():
+    return render_template("pages/opop/index.html")
+
+@app.route("/admin")
+@login_required
+def admin_page():
+    return render_template("pages/admin/index.html")
+
+@app.route("/teacher")
+@login_required
+def teacher_page():
+    return render_template("pages/teacher/index.html")
 
 @app.route("/")
 def home():
-    return render_template("login.html")
+    if current_user.is_authenticated:
+        return render_template("index.html")
+    else:
+        return render_template("login.html")
