307414bffd9a4f391c8975df1b3db20acf27225c
TotallyFuck <dan2004drug@yandex.ru>
2024-04-30 21:06:01 +0500

    Verstka website/css

--- fullstack/frontend/templates/pages/Teacher/js/script.js
+++ fullstack/frontend/templates/pages/Teacher/js/script.js
@@ -1,43 +1,43 @@
 
   // Получаем элементы списка и кнопку
-  const parent = document.getElementById('parent');
-  const child = document.getElementById('child');
-  const grandchild = document.getElementById('grandchild');
+  const school = document.getElementById('school');
+  const grupp = document.getElementById('grupp');
+  const student = document.getElementById('student');
 
   // Заполняем список детей данными в зависимости от выбранного родителя
-  const updateChildOptions = () => {
-    if (parent.value === '1') {
-      child.innerHTML = '<option value="1">Ребенок 1</option><option value="2">Ребенок 2</option>';
-    } else if (parent.value === '2') {
-      child.innerHTML = '<option value="3">1121Б</option><option value="4">1521Б</option>';
-    } else if (parent.value === '3') {
-      child.innerHTML = '<option value="5">Ребенок 5</option><option value="6">Ребенок 6</option>';
+  const updategruppOptions = () => {
+    if (school.value === '1') {
+      grupp.innerHTML = '<option value="1">Ребенок 1</option><option value="2">Ребенок 2</option>';
+    } else if (school.value === '2') {
+      grupp.innerHTML = '<option value="3">1121Б</option><option value="4">1521Б</option>';
+    } else if (school.value === '3') {
+      grupp.innerHTML = '<option value="5">Ребенок 5</option><option value="6">Ребенок 6</option>';
     }
-    child.disabled = false;
-    grandchild.disabled = true;
-    grandchild.innerHTML = '<option value="" selected>Выберите студента</option>';
+    grupp.disabled = false;
+    student.disabled = true;
+    student.innerHTML = '<option value="" selected>Выберите студента</option>';
   };
 
   // Заполняем список внуков данными в зависимости от выбранного ребенка
-  const updateGrandchildOptions = () => {
-    if (child.value === '1') {
-      grandchild.innerHTML = '<option value="1">Внук 1</option><option value="2">Внук 2</option>';
-    } else if (child.value === '2') {
-      grandchild.innerHTML = '<option value="3"></option>Внук 3<option value="4">Внук 4</option>';
-    } else if (child.value === '3') {
-      grandchild.innerHTML = '<option value="5">Сирченко михаих</option><option value="6">Малахин Антон</option>';
-    } else if (child.value === '4') {
-      grandchild.innerHTML = '<option value="7">Федосеев Павел</option><option value="8">Внук 8</option>';
-    } else if (child.value === '5') {
-      grandchild.innerHTML = '<option value="9">Внук 9</option><option value="10">Внук 10</option>';
-    } else if (child.value === '6') {
-      grandchild.innerHTML = '<option value="11">Внук 11</option><option value="12">Внук 12</option>';
+  const updatestudentOptions = () => {
+    if (grupp.value === '1') {
+      student.innerHTML = '<option value="1">Внук 1</option><option value="2">Внук 2</option>';
+    } else if (grupp.value === '2') {
+      student.innerHTML = '<option value="3"></option>Внук 3<option value="4">Внук 4</option>';
+    } else if (grupp.value === '3') {
+      student.innerHTML = '<option value="5">Сирченко михаих</option><option value="6">Малахин Антон</option>';
+    } else if (grupp.value === '4') {
+      student.innerHTML = '<option value="7">Федосеев Павел</option><option value="8">Внук 8</option>';
+    } else if (grupp.value === '5') {
+      student.innerHTML = '<option value="9">Внук 9</option><option value="10">Внук 10</option>';
+    } else if (grupp.value === '6') {
+      student.innerHTML = '<option value="11">Внук 11</option><option value="12">Внук 12</option>';
     }
-    grandchild.disabled = false;
+    student.disabled = false;
   };
 
   // Обрабатываем событие изменения родительского списка
-  parent.addEventListener('change', updateChildOptions);
+  school.addEventListener('change', updategruppOptions);
 
   // Обрабатываем событие изменения списка детей
-  child.addEventListener('change', updateGrandchildOptions);
+  grupp.addEventListener('change', updatestudentOptions);
