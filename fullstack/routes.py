16d9b763fbc4660a5b699e6a067d119d987a76cd
Kidcreole2 <131256373+Kidcreole2@users.noreply.github.com>
2024-05-03 03:55:19 +0500

    route changes continued

--- fullstack/routes.py
+++ fullstack/routes.py
@@ -8,129 +8,26 @@ from file_manager import allowed_file, save_file
 def loader_user(user_id):
     return Users.query.get(user_id) 
 
-@app.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for("login"))
+# ==admin folder functions==
 
-@app.route("/opop_index")
-@login_required
-def opop_index():
-    return render_template("pages/opop/index.html")
-
-@app.route("/opop_add_practice", methods=["GET","POST"])
-@login_required
-def opop_add_practice():
-    if request.method == "POST":
-        practice = Practice(
-            year=request.form['year'],
-            period_practice=request.form['periodPractice'],
-            name=request.form['namePractice'],
-            order=request.form['order'],
-            type_of_practice=request.form['typeOfPractice'],
-            kind_of_practice=request.form['kindOfPractice']
-        )
-        practice_id = Practice.add(practice)
-        specialization = request.form['specialization']
-        specialization_id = Specialization.query.filter_by(specialization=Specialization.name).first()
-        group = Group(
-            specialization_id=specialization_id,
-            name=request.form['nameGroup'],
-            course=request.form['course']
-        )
-        group_id = Group.add(group)
-        practice_group = Practice_Group(
-            practice_id=practice_id,
-            group_id=group_id
-        )
-        practice_group_id = Practice_Group.add(practice_group)
-    
-    messages = get_flashed_messages()
-    return render_template("pages/opop/add.html", messages=messages)
-
-@app.route("/opop_add_group", methods=["GET","POST"])
-@login_required
-def opop_add_practice():
-    if request.method == "POST":
-        specialization = request.form['specialization']
-        specialization = Specialization.query.filter_by(specialization=Specialization.name).first()
-        specialization_id = specialization.id
-        group = Group(
-            specialization_id=specialization_id,
-            name=request.form['nameGroup'],
-            course=request.form['course']
-        )
-    
-    messages = get_flashed_messages()
-    return render_template("pages/opop/add.html", messages=messages)
-
-@app.route("/opop_add_student", methods=["GET","POST"])
-@login_required
-def opop_add_student():
-    if request.method == "POST":
-        user_student = Users(
-            login=request.form['login'], 
-            password=request.form['password'], 
-            firstname=request.form['firstname'], 
-            lastname=request.form['lastname'], 
-            surname=request.form['surname'], 
-            role=request.form["role"]
-        )
-        user_student_id=Users.add(user_student)
-        group = request.form['group']
-        group = Group.query.filter_by(group=Group.name).first()
-        group_id = group.id
-        student = Student(
-            user_id=user_student_id,
-            group_id=group_id,
-            name_rp = request.form['nameRp']
-        )
-        student_id = Student.add(student)
-    
-    messages = get_flashed_messages()
-    return render_template("pages/opop/add.html", messages=messages)
+# --admin functions--
 
 @app.route("/admin_index")
 @login_required
 def admin_index():
-    users = db.session.execute(db.select(Users)
-            .order_by(Users.firstname)).scalars()
-    return render_template("pages/admin/index.html", users = users)
+    return render_template("pages/admin/index.html")
 
-@app.route("/admin_add_user", methods=["GET","POST"])
-def admin_add_data():
-    if request.method == "POST":
-        institute = Institute(
-            name=request.form['nameInstitute']
-        )
-        institute_id = Institute.add(institute)
-        user_opop = Users(
-            login=request.form['login'], 
-            password=request.form['password'], 
-            firstname=request.form['firstname'], 
-            lastname=request.form['lastname'], 
-            surname=request.form['surname'], 
-            role=request.form["role"]
-        )
-        user_opop_id = Users.add(user_opop)
-        opop = Director_OPOP(
-            user_id=user_opop_id,
-            post=request.form['post']
-        )
-        opop_id = Director_OPOP.add(opop)
-        specialization = Specialization(
-            institute_id=institute_id,
-            director_opop_id=opop_id,
-            name=request.form['nameSpecialization']
-        )
-        specialization_id = Specialization.add(specialization)
-    messages = get_flashed_messages()
-    return render_template("pages/admin/add.html", messages=messages)
+# --user functions--
 
-@app.route("/admin_add_user", methods=["GET","POST"])
+@app.route("/admin_user_index")
+@login_required
+def admin_user_index():
+    users = Users.query.order_by(Users.firstname).all()
+    return render_template("pages/admin/users/index.html", users=users)
+
+@app.route("/admin_user_create", methods=["GET","POST"])
 # @login_required
-def admin_add_user():
+def admin_user_create():
     if request.method == "POST":
         if request.form['password'] == request.form['passwordConfirm'] :
             user = Users(login=request.form['login'], 
@@ -147,7 +44,156 @@ def admin_add_user():
         else:
             flash("Пароли не совпадают")
     messages = get_flashed_messages()
-    return render_template("pages/admin/add.html", messages=messages)
+    return render_template("pages/admin/users/create.html", messages=messages)
+
+@app.route("/admin_user_update/<login>", methods=["GET","POST"])
+@login_required
+def admin_user_update(login):
+    old_user = Users.query.filter_by(login=login).first()
+    if request.method == "POST":
+        new_user = Users(login=request.form['login'], 
+                    password=request.form['password'], 
+                    firstname=request.form['firstname'], 
+                    lastname=request.form['lastname'], 
+                    surname=request.form['surname'], 
+                    role=request.form["role"]
+                    )
+        Users.update(old_user,new_user)
+    return render_template("pages/admin/users/update.html",old_user=old_user)
+
+# --institute functions--
+
+@app.route("/admin_institute_index")
+@login_required
+def admin_institute_index():
+    institutes = Institute.query.order_by(Institute.name).all()
+    return render_template("pages/admin/institute/index.html", institutes=institutes)
+
+
+@app.route("/admin_institute_create")
+@login_required
+def admin_institute_create():
+    if request.method == "POST":
+        new_institute = Institute(
+            name=request.form['name'],
+        )
+        Institute.create(new_institute)
+    return render_template("pages/admin/institute/create.html")
+
+@app.route("/admin_institute_update/<institute_id>")
+@login_required
+def admin_institute_update(institute_id):
+    old_institute = Institute.query.filter_by(id=institute_id).first()
+    if request.method == "POST":
+        new_institute = Institute(
+            name=request.form['name'],
+        )
+        Institute.update(old_institute,new_institute)
+    specializations = Specialization.query.order_by(Specialization.name).all()
+    return render_template("pages/admin/institute/update.html",old_institute=old_institute, specializations=specializations)
+
+# --specialization functions--
+
+@app.route("/admin_specialization_create/<institute_id>")
+@login_required
+def admin_specialization_create(institute_id):
+    if request.method == "POST":
+        new_specialization = Specialization(
+            name=request.form['name'],
+            specialization_code=request.form['specialization_code'],
+            institute_id=institute_id,
+            director_opop_id=request.form['director_opop_id']
+        )
+        Specialization.create(new_specialization)
+        return render_template("pages/admin/institute/specialization/update.html")
+    return render_template("pages/admin/institute/specialization/create.html")
+
+@app.route("/admin_specialization_update")
+@login_required
+def admin_specialization_update():
+    return render_template("pages/admin/institute/specialization/update.html")
+
+
+# ==opop functions folder==
+
+# --opop functions--
+
+@app.route("/opop_index")
+@login_required
+def opop_index():
+    return render_template("pages/opop/index.html")
+
+# --group functions--
+
+@app.route("/opop_group_index")
+@login_required
+def opop_group_index():
+    return render_template("pages/opop/group/index.html")
+
+
+@app.route("/opop_group_create")
+@login_required
+def opop_group_create():
+    return render_template("pages/opop/group/create.html")
+
+@app.route("/opop_group_update")
+@login_required
+def opop_group_update():
+    return render_template("pages/opop/group/update.html")
+
+# --student functions--
+
+@app.route("/opop_student_create")
+@login_required
+def opop_student_create():
+    return render_template("pages/opop/group/student/create.html")
+
+@app.route("/opop_student_update")
+@login_required
+def opop_student_update():
+    return render_template("pages/opop/group/student/update.html")
+
+# --practice functions--
+
+@app.route("/opop_practice_index")
+@login_required
+def opop_practice_index():
+    return render_template("pages/opop/practice/index.html")
+
+
+@app.route("/opop_practice_create")
+@login_required
+def opop_practice_create():
+    return render_template("pages/opop/practice/create.html")
+
+@app.route("/opop_practice_update")
+@login_required
+def opop_practice_update():
+    return render_template("pages/opop/practice/update.html")
+
+@app.route("/opop_practice_start")
+@login_required
+def opop_practice_start():
+    return render_template("pages/opop/practice/start.html")
+
+# ==student practice functions folder==
+
+@app.route("/studentPractice_student")
+@login_required
+def studentPractice_student():
+    return render_template("pages/studentPractice/student.html")
+
+@app.route("/studentPractice_supervisor")
+@login_required
+def studentPractice_supervisor():
+    return render_template("pages/studentPractice/supervisor.html")
+
+@app.route("/studentPractice_update")
+@login_required
+def studentPractice_update():
+    return render_template("pages/studentPractice/update.html")
+
+# ==utilite functions folder==
 
 @app.route("/upload_specs", methods=["GET", "POST"])
 @login_required
@@ -172,41 +218,23 @@ def upload():
     print(messages)
     return render_template("test/file_upload.html",  messages=messages)
 
-@app.route("/supervisorCompany_index")
-@login_required
-def supervisorCompany_index():
-    return render_template("pages/supervisorCompany/index.html")
 
-@app.route("/supervisorCompany_add")
-@login_required
-def supervisorCompany_add():
-    return render_template("pages/supervisorCompany/add.html")
+# ==Basic functions==
 
-@app.route("/supervisorPracticeOrg_index")
-@login_required
-def supervisorPracticeOrg_index():
-    return render_template("pages/supervisorPracticeOrg/index.html")
 
-@app.route("/supervisorPracticeOrg_add")
+@app.route("/logout")
 @login_required
-def supervisorPracticeOrg_add():
-    return render_template("pages/supervisorPracticeOrg/add.html")
-
-@app.route("/supervisorUgrasu_index")
-@login_required
-def supervisorUgrasu_index():
-    return render_template("pages/supervisorUgrasu/index.html")
-
-@app.route("/supervisorUgrasu_add")
-@login_required
-def supervisorUgrasu_add():
-    return render_template("pages/supervisorUgrasu/add.html")
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
 
 @app.route("/home")
 @login_required
 def home():
     roles = current_user.role.split()
     return render_template("index.html",roles=roles)
+
+# --login function--
 
 @app.route("/", methods=["GET", "POST"])
 def login():
