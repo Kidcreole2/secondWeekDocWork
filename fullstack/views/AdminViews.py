abe738d813e5c5c17c72c1a84c7f75ad03b591e1
bbdk612 <sircenkom239@gmail.com>
2024-05-06 15:25:47 +0500

    start

--- fullstack/views/AdminViews.py
+++ fullstack/views/AdminViews.py
@@ -1,3 +1,4 @@
+from flask_login import login_required
 from models import *
 
 @app.route("/admin")
@@ -7,5 +8,9 @@ def admin_index():
     institutes = Institute.query.all()
     return render_template("pages/admin/index.html", users=users, institutes=institutes)
 
-@app.route("/admin/<who>/<action>")
+@app.route("/admin/<entity>/<action>")
+@login_required
+def admin_entity_action(entity):
+
+
 @app.route("/admin/<who>/update/<who_id>")
