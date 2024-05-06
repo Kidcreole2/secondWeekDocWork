1f39f87ffd7c4cfcaea598a1a910195fadb9ac3a
bbdk612 <sircenkom239@gmail.com>
2024-05-06 21:47:40 +0500

    admin refactored

---
+++ fullstack/core/static/js/admin.index.js
@@ -0,0 +1,44 @@
+$(document).ready(() => {
+  $(".users-head-tools__button+.toggle").on("click", () => {
+    $(".users-list").toggle();
+    $(this).toggleClass("hided");
+  });
+
+  $(".institutes-head-tools__button+.toggle").on("click", () => {
+    $(".institutes-list").toggle();
+    $(this).toggleClass("hided");
+  });
+
+  $(".users-list-tools__button.delete").on("click", (e) => {
+    id = e.target.id;
+    $.ajax({
+      method: "POST",
+      url: `/admin/users/delete/${id}`,
+      dataType: "html",
+      success: () => {
+        $(`li#${id}`);
+        alert("Zaebis");
+      },
+      error: (xhr, status, error) => {
+        var err = "(" + xhr.responseText + ")";
+        alert(err);
+      },
+    });
+  });
+
+  $(".institutes-list-tools__button.delete").on("click", (e) => {
+    id = e.target.id;
+    $.ajax({
+      method: "POST",
+      url: `/admin/institutes/delete/${id}`,
+      dataType: "html",
+      success: () => {
+        alert("Zaebis");
+      },
+      error: (xhr, status, error) => {
+        var err = "(" + xhr.responseText + ")";
+        alert(err);
+      },
+    });
+  });
+});
