ee1de1ffa897743b48f682bbea6b1e4523bfb7bd
bbdk612 <sircenkom239@gmail.com>
2024-05-06 15:09:50 +0500

    start route refactor

--- fullstack/views/AdminViews.py
+++ fullstack/views/AdminViews.py
@@ -1,5 +1,11 @@
-@app.route("/admin_index")
+from models import *
+
+@app.route("/admin")
 @login_required
 def admin_index():
-    return render_template("pages/admin/index.html")
+    users = Users.query.all()
+    institutes = Institute.query.all()
+    return render_template("pages/admin/index.html", users=users, institutes=institutes)
 
+@app.route("/admin/<who>/<action>")
+@app.route("/admin/<who>/update/<who_id>")
