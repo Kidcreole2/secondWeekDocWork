b9945c05e04f5c5f4c660be1fb3839c10a0a2c57
bbdk612 <sircenkom239@gmail.com>
2024-05-10 01:21:13 +0500

    hot fix

--- fullstack/core/static/js/admin.spec-create.js
+++ fullstack/core/static/js/admin.spec-create.js
@@ -2,34 +2,18 @@ $(document).ready(() => {
   $("#specialisation-create").click(() => {
     $.ajax({
       method: "POST",
-      url: "/admin/specialization/name_check",
       dataType: "html",
       data: {
         name: $("input[name='name']").val(),
-        id: $("input[name='id']").val(),
+        code: $("input[name='code']").val(),
+        opop_id: $("select[name='director_opop_id'] option:selected").val(),
+        id: $("select[name='spec-institute'] option:selected").val(),
+      },
+      success: (data) => {
+        alert(JSON.parse(data).message);
       },
       error: (xhr, status, error) => {
         alert(JSON.parse(xhr.responseText).message);
-      },
-      success: () => {
-        $.ajax({
-          method: "POST",
-          dataType: "html",
-          data: {
-            name: $("input[name='name']").val(),
-            code: $("input[name='code']").val(),
-            opop_id: $("select[name='director_opop_id'] option:selected").val(),
-            id: $(
-              "select[name='spec-institute'] option:selected"
-            ).val(),
-          },
-          success: (data) => {
-            alert(JSON.parse(data).message);
-          },
-          error: (xhr, status, error) => {
-            alert(JSON.parse(xhr.responseText).message);
-          },
-        });
       },
     });
   });
